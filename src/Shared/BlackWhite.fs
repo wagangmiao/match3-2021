7853dc89749c9e314df836eaa89102e11852e0a1
xbfool <xbfool@gmail.com>
2020-12-13 22:01:59 +0800

    add blackwhite

---
+++ src/Shared/BlackWhite.fs
@@ -0,0 +1,42 @@
+﻿namespace Shared.Chess
+
+open System.Runtime.InteropServices
+open System.Xml
+
+module BlackWhite =
+    type Color =
+        |Black
+        |White
+
+    type ChessBoard =
+        {
+            height: int
+            width: int
+            board: Color[,]
+        }
+
+    type Action =
+        {
+            x: int
+            y: int
+            color: Color
+        }
+
+    type Change =
+        {
+            x: int
+            y: int
+            color: Color
+        }
+
+    type Turn =
+        {
+            turn_number: int
+            action: Action
+            changes: Change list
+        }
+
+    type Record =
+        {
+            turns: Turn list
+        }
